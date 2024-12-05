85715fe9079d70feb60c932f6b77fc6923dac2fc
dddijkstra <124427893+dddijkstra@users.noreply.github.com>
2024-12-05 13:19:25 +0800

    create a dir to scrap

---
+++ scrapy/config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    def __init__(self, result_file, user_data, web_path, time_sleep):
+        local_path = os.getcwd()
+        self.result_file_path = f"{local_path}/{result_file}"
+        self.user_data_dir = f"{local_path}/{user_data}"
+        self.web_path = web_path
+        self.time_sleep = time_sleep
---
+++ scrapy/containers.py
@@ -0,0 +1,11 @@
+class NewContainer:
+    def __init__(self):
+        self.n_p = 0
+        self.new_current_page_number = 1
+        self.new_max_page = 20
+
+class Container:
+    def __init__(self):
+        self.p = 0
+        self.current_page_number = 1
+        self.max_page = 0
